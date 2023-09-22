a522b474b14d0ca9583cd5c7e3536e088de0e7ab
raydeleu <38007983+raydeleu@users.noreply.github.com>
2023-09-22 10:50:52 +0200

    Add files via upload

--- models/hexagon_holder_v3.js
+++ models/hexagon_holder_v3.js
@@ -1,22 +1,27 @@
+// Experiment to create a holder for GPS Receiver
+// added code to create honeycomb pattern 
+// within a rectangular shape
 
 const r = replicad
 
 const main = () => {
 
-  let lx = 45.25    // width of gns receiver
-  let ly = 79.25;   // length of gns receiver
-  let lz = 11.4;    // thickness of gns receiver
-  let lt = 0.5 ;    // tolerance for fit around the receiver    
-  let th = 2   ;    // thickness of holder around the receiver
+  // Dimensions of the GPS receiver
+  let lx = 45.25;      // width of gns receiver
+  let ly = 79.25;      // length of gns receiver
+  let lz = 11.4;       // thickness of gns receiver
+  let lt = 0.5 ;       // tolerance for fit around the receiver    
   
+  let th = 2   ;       // thickness of holder around the receiver
   let wholder = 20  ;    // width of lanyard holder
   let yholder = 10  ;    // amount that holder sticks out of body
 
   let rlanhol = 2   ;    // radius of lanyard hole
-  let ycut   = 0.6  ; // portion of side length to be cut
+  let ycut    = 0.6  ; // portion of side length to be cut
 
-  let rlandist = 7  ; // distance between two holes for lanyard, set to 0 for single hole  
-
+  let rlandist = 0  ; // distance between two holes for lanyard, set to 0 for single hole  
+  
+  // code to create a hexagon face 
   function Hexagon(size)
   { 
     let sketchHexagon 
@@ -37,13 +42,11 @@ const main = () => {
     return sketchHexagon.close()
     }
 
+    // code to create a hexagonal column, adding height to hexagon face
     function hexColumn(size,height)
     {
       return Hexagon(size).extrude(height);
     }
-
-
-
 
   // add tolerances to the dimensions of object to be held
   lx = lx + lt; 
@@ -65,6 +68,7 @@ const main = () => {
   let shape = r.makeBaseBox(lx+2*th,ly+2*th,lz+2*th)
   .fillet((lz+2*th-lt)/2,(e)=>e.inDirection("Y").inPlane("XY",lz+2*th))
 
+  // create a lip on the holder to attach it to a lanyard
   let lanyardholder = r.makeBaseBox(wholder,ly+(2*th),th).translate([0,yholder,0])
   shape = shape.fuse(lanyardholder)
   
@@ -82,24 +86,33 @@ const main = () => {
   let shapeRounded = shapeUnrounded
   .fillet(1.0,(e)=>e.containsPoint([0, ly*ycut/2 , lz+2*th]))
 
-  // to round the holder for the lanyard we combine two finders 
-  // selecting first the edges in the z direction and the out of 
+  // to round the lip for the lanyard we combine two finders 
+  // selecting first the edges in the z direction,  out of 
   // these only select the ones at the proper distance
   shapeRounded = shapeRounded.fillet(8,(e)=>e.inDirection("Z").inPlane("XZ",-(((ly+2*th)/2)+yholder)))
  
+  // punch two holes in the lip, with distance rlanhol 0 it becomes one hole
   let lanyardCutterL = r.makeCylinder(rlanhol,th*4,[rlandist/2,ly/2+yholder/2+th,-2*th],[0,0,1])
   let lanyardCutterR = r.makeCylinder(rlanhol,th*4,[-rlandist/2,ly/2+yholder/2+th,-2*th],[0,0,1])
   shapeRounded = shapeRounded.cut(lanyardCutterL)
   shapeRounded = shapeRounded.cut(lanyardCutterR)
 
   // now cut the top part of the holder 
-  // as this overhang is difficult for the 3D printer,
+  // as a closed shape is difficult for the 3D printer,
   // then  round all remaining edges with a smaller radius
-  shapeRounded = shapeRounded.cut(cutterTop).fillet(0.6)
+  // used a chamfer instead of fillet(0.6)
+  shapeRounded = shapeRounded.cut(cutterTop).chamfer(0.4)
 
-  
+  // dimensions of honeycomb grid 
+  let hc_width  = 35;
+  let hc_length = 65;
+  let hc_depth  = 10;  
+  // note that hexagons are defined at plane XY, -1
+
+  // number of rows and columns from center of rectangle 
   let rowNumber = 5;
   let colNumber = 2;
+  // cellsize and wallthickness of honeycomb 
   let wallThickness = 1;
   let cellSize = 5;
     
@@ -107,10 +120,7 @@ const main = () => {
   let delta_x = (1+Math.sin(deg30)) * cellSize + wallThickness*Math.cos(deg30)
   let delta_y = 0.5*wallThickness + Math.cos(deg30)*cellSize
 
-  // draw a box to subtract the hexagonal columns that are created below 
-  let cutterBox = r.makeBaseBox(40,70,10)
-  //let hexColumnGrid = hexColumn(cellSize,5*lt);
-  
+ 
   let point = [];
   let cutColumn;
   
@@ -118,6 +128,9 @@ const main = () => {
     {
       for (let colCount = 1 ;  colCount <= colNumber ; colCount += 1)
           {
+          // two cells are defined and then replicated in each quadrant 
+          // around the center of the grid, so 8 holes are defined each time 
+          // This way the grid is always nicely symmetrical. 
           point[1] = [(colCount-1) * 2 * delta_x,(rowCount-1) * delta_y * 2 ,0];
           point[2] = [-(colCount-1) * 2 * delta_x,(rowCount-1) * delta_y * 2 ,0];
           point[3]= [(colCount-1) * 2 * delta_x,-(rowCount-1) * delta_y * 2 ,0];
@@ -127,22 +140,22 @@ const main = () => {
           point[7] = [(((colCount-1)*2)+1) * delta_x, -(rowCount-1)*delta_y*2+delta_y,0];
           point[8] = [(((colCount-1)*2)+1) * -delta_x, -(rowCount-1)*delta_y*2+delta_y,0];
           for (let j=1; j<=8; j+=1)
-          { 
-            cutColumn = hexColumn(cellSize,5*th).translate(point[j]);
-            cutColumn = cutColumn.intersect(r.makeBaseBox(35,65,10)) 
-            shapeRounded = shapeRounded.cut(cutColumn);
+            { 
+              cutColumn = hexColumn(cellSize,5*th).translate(point[j]);
+              // the defined column is intersected with a box to limit the grid 
+              // within a rectangular shape
+              cutColumn = cutColumn.intersect(r.makeBaseBox(hc_width,hc_length,hc_depth)) 
+              shapeRounded = shapeRounded.cut(cutColumn);
+            }
           }
-          
-  }
-  }
+    }
   
   let shapeArray = 
   [ 
   {shape: receiver, name: "receiver", color: "dimgrey", opacity: 0.8},   
-  {shape: shapeRounded, name: "holder", color: "steelblue", opacity: 1.0},
-  {shape: cutterBox}
+  {shape: shapeRounded, name: "holder", color: "steelblue", opacity: 1.0}
   ]
     
 return shapeArray
   
-  }
+}
